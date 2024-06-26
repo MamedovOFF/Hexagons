b847752cbd398e18e74ad23864fa6dd3047f70a8
M_dev <mamedov_dev@mail.ru>
2024-06-26 14:59:25 +0300

    Add Touch Handlers

--- src/components/Hexagons/index.tsx
+++ src/components/Hexagons/index.tsx
@@ -1,4 +1,4 @@
-import { useState, WheelEventHandler } from 'react'
+import { TouchEventHandler, useState, WheelEventHandler } from 'react'
 import './style.css'
 
 type hexagonsProps = {
@@ -19,6 +19,8 @@ const calculateStyle = (active: number, idx: number) => {
   }
 }
 
+let startX = 0
+
 function Hexagons({ hexagons }: hexagonsProps) {
   const [active, setActive] = useState(Math.floor((hexagons.length - 1) / 2))
 
@@ -30,12 +32,28 @@ function Hexagons({ hexagons }: hexagonsProps) {
     }
   }
 
+  const handleTouchStart: TouchEventHandler<HTMLDivElement> = (event) => {
+    startX = event.touches[0].clientX
+  }
+
+  const handleTouchEnd: TouchEventHandler<HTMLDivElement> = (event) => {
+    const endX = event.changedTouches[0].clientX
+    if (endX > startX && active > 0) setActive(active - 1)
+    else if (endX < startX && active < hexagons.length - 1)
+      setActive(active + 1)
+  }
+
   const moveToItem = (id: number) => {
     setActive(id)
   }
 
   return (
-    <div className="carousel" onWheel={handelWheel}>
+    <div
+      className="carousel"
+      onWheel={handelWheel}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+    >
       <div className="opponent opponent-1">
         <span>{hexagons[active].opponent_1}</span>
       </div>
